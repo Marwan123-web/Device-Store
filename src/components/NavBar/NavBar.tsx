65e8fcece9fef8a48b71a810cc8bc4bf65681a24
marwan123-web <marawansalman98@gmail.com>
2023-04-17 17:51:43 +0200

    updated navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,21 +1,22 @@
 import FetchHook from "../../hooks/FetchHook";
 import { Link, NavLink } from "react-router-dom";
-import { LocalDataDI } from "../../models/localData.model";
+import { LocalDataDI, LocalDataI } from "../../models/localData.interface";
+import { FaShoppingCart } from "react-icons/fa";
 
 const NavBar = () => {
   const id: any = "Navbar";
-  const navs = FetchHook(id);
+  const navs: any = FetchHook(id);
   return (
     <div className="shadow-lg backdrop-blur-lg py-5 text-gray-900 bg-gray-50">
       <nav className="flex items-center container mx-auto">
         <div>
           <Link to="/" className="text-gray-700 italic text-7xl">
-            Device Store
+            DS
           </Link>
         </div>
         <ul className="list-none flex justify-center items-center ml-auto gap-5">
           {navs?.data?.map((nav: LocalDataDI) => (
-            <li>
+            <li key={nav.id}>
               <NavLink to={nav?.url}>
                 {nav?.icon ? nav?.icon : nav?.name}
               </NavLink>
