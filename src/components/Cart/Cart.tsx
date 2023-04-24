002840d70d0d1408b0a06d8d83f834736e925fde
marwan123-web <marawansalman98@gmail.com>
2023-04-24 05:32:37 +0200

    translated cart page

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -5,9 +5,19 @@ import { ProductI } from "../../models/products.interface";
 import Button from "../Shared/Button";
 import { deleteItem, editItem } from "../../redux/cart/slice";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 const Cart = () => {
+  const { t } = useTranslation("common");
+
   const dispatch = useDispatch();
   const cart = useSelector((state: any) => state.cart.items);
+  if (!cart.length) {
+    return (
+      <section>
+        <Herotext text={"Cart Page"} bg={"cartbg"} />
+      </section>
+    );
+  }
   return (
     <section>
       <Herotext text={"Cart Page"} bg={"cartbg"} />
@@ -15,21 +25,23 @@ const Cart = () => {
         <div className="flex shadow-md my-10">
           <div className="w-3/4 bg-white px-10 py-10">
             <div className="flex justify-between border-b pb-8">
-              <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-              <h2 className="font-semibold text-2xl">{cart.length} Items</h2>
+              <h1 className="font-semibold text-2xl">{t("shoppingCart")}</h1>
+              <h2 className="font-semibold text-2xl">
+                {cart.length} {t("items")}
+              </h2>
             </div>
             <div className="flex mt-10 mb-5">
               <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">
-                Product Details
+                {t("productDetails")}
               </h3>
               <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">
-                Quantity
+                {t("quantity")}
               </h3>
               <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">
-                Price
+                {t("price")}
               </h3>
               <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">
-                Total
+                {t("total")}
               </h3>
             </div>
             {cart.map((item: ProductI, index: number) => (
@@ -39,19 +51,23 @@ const Cart = () => {
               >
                 <div className="flex w-2/5">
                   <div className="w-20">
-                    {/* <img className="h-24" src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z" alt=""> */}
+                    <Link to={"/product/" + item.title}>
+                      <img className="h-24" src={item.img} alt="" />
+                    </Link>
                   </div>
                   <div className="flex flex-col justify-between ml-4 flex-grow">
-                    <span className="font-bold text-sm">{item.title}</span>
+                    <span className="font-bold text-sm">
+                      <Link to={"/product/" + item.title}>{item.title}</Link>
+                    </span>
                     <span className="text-red-500 text-xs">
                       {item.category}
                     </span>
-                    <a
+                    <span
                       className="font-semibold hover:text-red-500 text-gray-500 text-xs"
                       onClick={() => dispatch(deleteItem(item as any))}
                     >
-                      Remove
-                    </a>
+                      {t("remove")}
+                    </span>
                   </div>
                 </div>
                 <div className="flex justify-center w-1/5">
@@ -98,13 +114,7 @@ const Cart = () => {
               to="/"
               className="flex font-semibold text-indigo-600 text-sm mt-10"
             >
-              <svg
-                className="fill-current mr-2 text-indigo-600 w-4"
-                viewBox="0 0 448 512"
-              >
-                <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
-              </svg>
-              Continue Shopping
+              {t("continueShopping")}
             </Link>
           </div>
 
