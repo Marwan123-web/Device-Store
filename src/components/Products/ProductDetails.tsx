a93cc52eedb7c81846f8115808fa39758eaf74e2
marwan123-web <marawansalman98@gmail.com>
2023-04-27 02:08:26 +0200

    implement  react-helmet for seo

--- src/components/Products/ProductDetails.tsx
+++ src/components/Products/ProductDetails.tsx
@@ -5,6 +5,7 @@ import { ProductI } from "../../models/products.interface";
 import Button from "../Shared/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, editItem } from "../../redux/cart/slice";
+import Seo from "../Shared/Seo";
 
 const ProductDetails = () => {
   const params = useParams();
@@ -40,106 +41,115 @@ const ProductDetails = () => {
       </p>
     );
   return (
-    <section className="flex flex-col gap-16 py-10 bg-gray-100">
-      <div className="container mx-auto flex justify-around  items-center w-[80%] lg:flex-nowrap flex-wrap">
-        <div className="w-96 flex justify-end">
-          <img
-            src={product?.img}
-            alt={product?.title}
-            className="w-full select-none"
-          />
-        </div>
-        <div className="flex flex-col gap-3">
-          <p className="text-gray-500">
-            {"Home/"}
-            {<Link to="/product">product</Link>}
-            {`/${product?.title}`}
-          </p>
-          <h2 className="text-4xl">{product?.title.slice(0, 30)}</h2>
-          <span className="font-semibold">
-            Price: <span className="text-2xl">{product?.price}</span>
-          </span>
-          <span className="font-semibold">Brand: {product?.brand}</span>
-          <div className="flex flex-col gap-2">
-            <h1 className="text-2xl">Key features</h1>
-            <p className="text-gray-800">{product?.description.slice(0, 35)}</p>
-            <p className="text-gray-800">
-              {product?.description.slice(36, 70)}
-            </p>
-            <p className="text-gray-800">
-              {product?.description.slice(71, 100)}
-            </p>
-            <p className="text-gray-800">
-              {product?.description.slice(101, 130)}
-            </p>
-            <p className="text-gray-800">
-              {product?.description.slice(131, 170)}
-            </p>
-            <p className="text-gray-800">
-              {product?.description.slice(170, 201)}
-            </p>
-          </div>
-          <h3 className="flex justify-between text-gray-700 text-lg">
-            <span>Category: {product?.category}</span>
-            <span>
-              Rating:{" "}
-              <span className="text-rose-500 font-bold">
-                {product?.rating.slice(0, 3)}
-              </span>
-              <span>{product?.rating.slice(3)}</span>
-            </span>
-          </h3>
-          {!foundInCart && (
-            <Button
-              ButtonFun={() => dispatch(addItem(product as any))}
-              label={"add to cart"}
-              classes={
-                "bg-sky-400 text-sky-50 hover:bg-sky-50 hover:text-sky-400 duration-300 border border-sky-400 px-2 py-1 rounded-md"
-              }
+    <>
+      <Seo
+        description={product?.description}
+        title={product?.title}
+        image={product?.img}
+      />
+      <section className="flex flex-col gap-16 py-10 bg-gray-100">
+        <div className="container mx-auto flex justify-around  items-center w-[80%] lg:flex-nowrap flex-wrap">
+          <div className="w-96 flex justify-end">
+            <img
+              src={product?.img}
+              alt={product?.title}
+              className="w-full select-none"
             />
-          )}
-          {foundInCart && (
-            <div className="counter text-center">
-              <Button
-                label={"-"}
-                classes={
-                  "bg-sky-400 text-sky-50 hover:bg-sky-50 hover:text-sky-400 duration-300 border border-sky-400 px-2 py-1 rounded-md mx-3"
-                }
-                ButtonFun={() =>
-                  dispatch(
-                    editItem({
-                      ...product,
-                      method: "remove",
-                    } as any)
-                  )
-                }
-              />
-              <span>{foundInCart?.quantity}</span>
-              <Button
-                label={"+"}
-                classes={
-                  "bg-sky-400 text-sky-50 hover:bg-sky-50 hover:text-sky-400 duration-300 border border-sky-400 px-2 py-1 rounded-md mx-3"
-                }
-                ButtonFun={() =>
-                  dispatch(
-                    editItem({
-                      ...product,
-                      method: "add",
-                    } as any)
-                  )
-                }
-              />
+          </div>
+          <div className="flex flex-col gap-3">
+            <p className="text-gray-500">
+              {"Home/"}
+              {<Link to="/product">product</Link>}
+              {`/${product?.title}`}
+            </p>
+            <h2 className="text-4xl">{product?.title.slice(0, 30)}</h2>
+            <span className="font-semibold">
+              Price: <span className="text-2xl">{product?.price}</span>
+            </span>
+            <span className="font-semibold">Brand: {product?.brand}</span>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl">Key features</h1>
+              <p className="text-gray-800">
+                {product?.description.slice(0, 35)}
+              </p>
+              <p className="text-gray-800">
+                {product?.description.slice(36, 70)}
+              </p>
+              <p className="text-gray-800">
+                {product?.description.slice(71, 100)}
+              </p>
+              <p className="text-gray-800">
+                {product?.description.slice(101, 130)}
+              </p>
+              <p className="text-gray-800">
+                {product?.description.slice(131, 170)}
+              </p>
+              <p className="text-gray-800">
+                {product?.description.slice(170, 201)}
+              </p>
             </div>
-          )}{" "}
+            <h3 className="flex justify-between text-gray-700 text-lg">
+              <span>Category: {product?.category}</span>
+              <span>
+                Rating:{" "}
+                <span className="text-rose-500 font-bold">
+                  {product?.rating.slice(0, 3)}
+                </span>
+                <span>{product?.rating.slice(3)}</span>
+              </span>
+            </h3>
+            {!foundInCart && (
+              <Button
+                ButtonFun={() => dispatch(addItem(product as any))}
+                label={"add to cart"}
+                classes={
+                  "bg-sky-400 text-sky-50 hover:bg-sky-50 hover:text-sky-400 duration-300 border border-sky-400 px-2 py-1 rounded-md"
+                }
+              />
+            )}
+            {foundInCart && (
+              <div className="counter text-center">
+                <Button
+                  label={"-"}
+                  classes={
+                    "bg-sky-400 text-sky-50 hover:bg-sky-50 hover:text-sky-400 duration-300 border border-sky-400 px-2 py-1 rounded-md mx-3"
+                  }
+                  ButtonFun={() =>
+                    dispatch(
+                      editItem({
+                        ...product,
+                        method: "remove",
+                      } as any)
+                    )
+                  }
+                />
+                <span>{foundInCart?.quantity}</span>
+                <Button
+                  label={"+"}
+                  classes={
+                    "bg-sky-400 text-sky-50 hover:bg-sky-50 hover:text-sky-400 duration-300 border border-sky-400 px-2 py-1 rounded-md mx-3"
+                  }
+                  ButtonFun={() =>
+                    dispatch(
+                      editItem({
+                        ...product,
+                        method: "add",
+                      } as any)
+                    )
+                  }
+                />
+              </div>
+            )}{" "}
+          </div>
         </div>
-      </div>
-      <Link
-        to="/product"
-        className="text-xl py-1 text-center hover:text-cyan-500 duration-300 select-none"
-      >
-        &larr; Go to Product
-      </Link>
-    </section>
+        <Link
+          to="/product"
+          className="text-xl py-1 text-center hover:text-cyan-500 duration-300 select-none"
+        >
+          &larr; Go to Product
+        </Link>
+      </section>
+    </>
   );
 };
 
